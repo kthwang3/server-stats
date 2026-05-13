d71a8e3393ca3f0f0813af56d143e0ee17347c67
kthwang3 <kthwang3@gmail.com>
2026-05-12 19:53:17 -0700

    added additional performance stats

--- server-stats.sh
+++ server-stats.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+#AUTHOR: KYLE WANG
+#A SCRIPT THAT DISPLAYS SERVER PERFORMANCE STATS
+#5/12/2026
 echo "=== CPU USAGE ==="
 top -bn1 | grep "Cpu" | awk -F':' '{print $2}' | awk -F',' '{print $1, $2, $4}'
 echo "===MEMORY USAGE==="
@@ -11,3 +16,15 @@ ps aux | sort -nr -k3,3 | head -n 5 | awk '{print $1, $3, $11}'
 echo "=== TOP 5 MEMORY PROCESSES ==="
 echo "USER %MEM COMMAND"
 ps aux | sort -nr -k4,4 | head -n 5 | awk '{print $1, $4, $11}'
+echo "=== OS VERSION ==="
+cat /etc/os-release | grep "VERSION_ID" | awk -F'"' '{print $2}'
+echo "=== UPTIME ==="
+uptime -p
+echo "=== LOGGED IN USERS ==="
+uptime | grep -oE "[0-9]+ users" 
+echo "=== LOAD AVERAGES ==="
+uptime | awk -F'[a-z]:' '{ print $2 }'
+echo "=== NUMBER OF FAILED LOGIN ATTEMPTS ==="
+sudo grep -c "Failed password" /var/log/auth.log
+
+
