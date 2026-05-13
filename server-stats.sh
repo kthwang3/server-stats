6e38892f22d7e1eff28b4c9fdae7c3aced4242f6
kthwang3 <kthwang3@gmail.com>
2026-05-12 18:48:45 -0700

    added top cpu processes

--- server-stats.sh
+++ server-stats.sh
@@ -3,3 +3,9 @@ top -bn1 | grep "Cpu" | awk -F':' '{print $2}' | awk -F',' '{print $1, $2, $4}'
 echo "===MEMORY USAGE==="
 MEMORY_LINE=$(free -h | grep "Mem")
 echo $MEMORY_LINE | awk '{print "Total: "$2, "Used: "$3, "Free: "$4}'
+echo "===DISK USAGE==="
+df -h | grep "/$" | awk '{print "Total: "$2, "Used: "$3, "Free: "$4}'
+echo "=== TOP 5 CPU PROCESSES ==="
+echo "USER %CPU COMMAND"
+ps aux | sort -nr -k3,3 | head -n 5 | awk '{print $1, $3, $11}'    
+
