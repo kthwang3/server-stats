f406cc46e383d39ac69404b8a4bc15db80a31557
kthwang3 <kthwang3@gmail.com>
2026-05-12 17:36:28 -0700

    added memory line

--- server-stats.sh
+++ server-stats.sh
@@ -1,2 +1,10 @@
 echo "=== CPU USAGE ==="
 top -bn1 | grep "Cpu" | awk -F':' '{print $2}' | awk -F',' '{print $1, $2, $4}'
+echo "===MEMORY USAGE==="
+MEMORY_LINE=$(free -h | grep "Mem")
+echo "===TOTAL MEMORY==="
+echo $MEMORY_LINE | awk '{print $2}'
+echo "===USED MEMORY==="
+echo $MEMORY_LINE | awk '{print $3}' 
+echo "===FREE MEMORY==="
+echo $MEMORY_LINE | awk '{print $4}' 
